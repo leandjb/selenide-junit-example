1021ce48f4374686f1f5c18fb4b919bd812f091f
leandjb <leandjb@gmail.com>
2023-12-30 11:08:41 -0500

    add Junit text report

--- src/test/java/com/orangehrm/tests/AddEmployeeTest.java
+++ src/test/java/com/orangehrm/tests/AddEmployeeTest.java
@@ -1,10 +1,13 @@
 package com.orangehrm.tests;
 
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.junit5.TextReportExtension;
 import com.orangehrm.pages.EmployeeInformationPage;
 import com.orangehrm.pages.LoginPage;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith(TextReportExtension.class)
 class AddEmployeeTest {
 
     @Test
